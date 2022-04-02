54ca2d4f6b4dae7ee1e86d9a3611e9e7b5001d03
guille <exarodriguez@gmail.com>
2022-04-02 20:30:14 -0300

    initial source code

---
+++ app/src/main/java/com/example/admedia/AdmediaApplication.java
@@ -0,0 +1,13 @@
+package com.example.admedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmediaApplication.class, args);
+	}
+
+}
